5d6d7eb9008f5b28976b1aa308b2990e80baf1e8
alan-nyled <alannyled@gmail.com>
2022-03-17 15:48:56 +0100

    Første forsøg....virker ikke

---
+++ Dashboard/Services/IPowerPriceService.cs
@@ -0,0 +1,9 @@
+﻿using DataModels;
+
+namespace Dashboard.Services
+{
+    public interface IPowerPriceService
+    {
+        Task<IEnumerable<PowerPriceModel>> getPowerPrice();
+    }
+}
---
+++ DataModels/PowerPriceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class PowerPriceModel
+    {
+        public DateTime HourDK { get; set; }
+        public double SpotPriceDKK { get; set; }
+    }
+}
